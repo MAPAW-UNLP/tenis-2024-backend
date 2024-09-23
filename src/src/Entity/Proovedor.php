2ecd794ae68608ca2d5936ce6c83b9795cedd0a7
Luis Emanuel Banega <emabanega@hotmail.com>
2024-09-23 14:47:17 -0300

    Creo la entidad Proovedor

---
+++ src/src/Entity/Proovedor.php
@@ -0,0 +1,13 @@
+class Proovedor 
+{
+    private $id
+    private $nombre;
+    private $telefono;
+
+    public function getId(): ?int
+    {
+        return $id
+    }
+
+    public fuction getNombre(): ?str
+}
