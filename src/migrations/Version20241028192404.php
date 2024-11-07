d80b7616f2d1d98e796870a82dace90a4b087a30
Lauty1550 <lautyjaime09@gmail.com>
2024-11-06 23:12:43 -0300

    Se creaba una clase de mas

--- src/migrations/Version20241028192404.php
+++ src/migrations/Version20241028192404.php
@@ -21,7 +21,6 @@ final class Version20241028192404 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE item_alquiler (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(50) NOT NULL, importe INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('INSERT INTO clases (tipo, importe) values ("GRUPAL", 50)');
         $this->addSql('INSERT INTO item_alquiler (description, importe) values ("RAQUETA", 2000)');
         $this->addSql('INSERT INTO item_alquiler (description, importe) values ("PELOTA", 100)');
     }
